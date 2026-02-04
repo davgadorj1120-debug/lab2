6e7d764b34cda27c2746d250e080ff6f3b7c4d46
Davka Davka <chi@example.com>
2026-02-04 15:54:34 +0800

    Fix Allowed Hosts error

--- lab2/settings.py
+++ lab2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*4^(9@h+*#6ala34qw1e976)@%aizsonspz!c&(6xcca9kh=&^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
